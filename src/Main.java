983371bef2522ad2994922eb93f03c82c6ad408b
tannnnnmay <tanmay.rewatkar@gmail.com>
2024-02-24 11:37:37 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Tanmay");
         System.out.println("Hello world!");
+
+        System.out.println("Njhgjdhakj");
     }
 }
